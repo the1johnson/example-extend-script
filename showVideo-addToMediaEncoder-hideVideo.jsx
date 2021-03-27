0ec92f3d45f847a0af1fb4e542d730718e415b68
John Johnson <john@the1johnson.com>
2021-03-27 10:05:27 -0600

    Added first extend script example

---
+++ showVideo-addToMediaEncoder-hideVideo.jsx
@@ -0,0 +1,13 @@
+﻿var compItem = app.project.item(1);
+var testCnt = 9;
+var layerCnt = compItem.numLayers;
+
+if(compItem.name === 'speed40'){
+    for( var q = 1; q < testCnt; q++){
+        if(compItem.layer(q) instanceof AVLayer){
+            compItem.layer(q).enabled = true;
+            app.executeCommand(3800);
+            compItem.layer(q).enabled = false;
+        }
+    }
+}
